28ac1b33717f5d2db8ff9c67502d184dc81fd620
Tejas Shah <tejas.s@browserstack.com>
2017-07-26 14:48:41 +0530

    upgrade config to rails 4

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.9'
+gem 'rails', '4.1.15'
+gem 'prototype-rails'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -8,20 +9,22 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160517125340) do
+ActiveRecord::Schema.define(version: 20160517125340) do
 
-  create_table "documents", :force => true do |t|
+  create_table "documents", force: true do |t|
     t.string   "title"
     t.string   "doc_path"
-    t.boolean  "is_private", :default => true
+    t.boolean  "is_private", default: true
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
+  add_index "documents", ["user_id"], name: "index_documents_on_user_id"
+
+  create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
